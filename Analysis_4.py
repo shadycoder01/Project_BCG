c3fcb26fa7342ee2049fb8c1364641616ef25ca2
shadycoder01 <103529246+shadycoder01@users.noreply.github.com>
2023-01-19 13:21:13 +0530

    Update Analysis_4.py

--- Analysis_4.py
+++ Analysis_4.py
@@ -18,8 +18,8 @@ pd.set_option('display.max_columns',None,'display.max_rows',100)
 
 class main:
     def __init__(self):
-        self.input_path = configuration.Configuration(os.getcwd(),'Data','Case_Study').get_units_use_path()
-        self.output_path = configuration.Configuration(os.getcwd(),'Data','Case_Study').output_path_analysis_4()
+        self.input_path = configuration.Configuration(os.getcwd(),'input_data','output_data').get_units_use_path()
+        self.output_path = configuration.Configuration(os.getcwd(),'input_data','output_data').output_path_analysis_4()
         
     def analysis_4(self): 
         df_unit_use = pd.read_csv(self.input_path)
